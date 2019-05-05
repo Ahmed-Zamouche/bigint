986f7fa318b6df749e88c70955659d11a22d385e
Ahmed Zamouche <ahmed.zamouche@gmail.com>
2019-05-05 15:20:01 +0200

    Use make

--- build.bat
+++
@@ -1,9 +0,0 @@
-echo off
-
-rem g++ -O3 -Wall --std=c++11 -c .\bigint.cpp -o .\bigint.o
-
-rem g++ -O3 -Wall --std=c++11 .\bigint.o .\TEST_bigint.cpp -o .\TEST_bigint
-
-.\TEST_bigint > result.js
-
-node result.js
